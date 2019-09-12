6ac63a1fc4693196b7183f0eae3e208a2d90301a
holichang <1185381333@qq.com>
2019-09-12 21:50:04 +0800

    add D22-D24

---
+++ D22-D24/hmk.js
@@ -0,0 +1,289 @@
+let show=document.getElementById("result1");
+let inputA=document.getElementById("num-a");
+let inputB=document.getElementById("num-b");
+let inputStrA=document.getElementById("numstr-a");
+let inputStrB=document.getElementById("numstr-b");
+let textA=document.getElementById("str-a");
+let textB=document.getElementById("str-b");
+let show2=document.getElementById("result2")
+/*引用类型的赋值传的是指针*/
+function isNumber(number){
+	if(number!=null&&number!=""){
+		if(isNaN(number)){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	else{
+		return false;
+	}
+}
+function isNum(){
+	let myRadio=document.querySelector(".num input:checked").value;
+    let num=document.getElementById("num-"+myRadio).value;
+	if(isNumber(num)){
+		show.innerHTML="当前输入框的内容是数字";
+	}
+	else{
+		show.innerHTML="当前输入框的内容不是数字";
+	}
+}
+function toChange(){
+	let numA=inputA.value;
+	let numB=inputB.value;
+	if(isNumber(numA)&&isNumber(numB)){
+		numA=Number(numA);
+		numB=Number(numB).toFixed(0);
+		if(0<=numB&&numB<=20){
+			show.innerHTML=numA.toFixed(numB);
+		}
+		else{
+			console.log("小数位数不能超过指定位数")
+		}
+	}
+	else{
+		console.log("输入框的内容不都为数字")
+	}
+}
+function toAbs(){
+	let myRadio=document.querySelector(".num input:checked").value;
+    let num=document.getElementById("num-"+myRadio).value;
+	if(isNumber(num)){
+		num=Number(num);
+		show.innerHTML=Math.abs(num);
+	}
+	else
+	{
+		show.innerHTML="当前输入框的内容不是数字";
+	}
+}
+function toUp(){
+	let myRadio=document.querySelector(".num input:checked").value;
+    let num=document.getElementById("num-"+myRadio).value;
+	if(isNumber(num)){
+		num=Number(num);
+		show.innerHTML=Math.ceil(num);
+	}
+	else
+	{
+		show.innerHTML="当前输入框的内容不是数字";
+	}
+}
+function toDown(){
+	let myRadio=document.querySelector(".num input:checked").value;
+    let num=document.getElementById("num-"+myRadio).value;
+	if(isNumber(num)){
+		num=Number(num);
+		show.innerHTML=Math.floor(num);
+	}
+	else
+	{
+		show.innerHTML="当前输入框的内容不是数字";
+	}
+}
+function toMiddle(){
+	let myRadio=document.querySelector(".num input:checked").value;
+    let num=document.getElementById("num-"+myRadio).value;
+	if(isNumber(num)){
+		num=Number(num);
+		show.innerHTML=num.toFixed(0);
+	}
+	else{
+		show.innerHTML="当前输入框的内容不是数字";
+	}
+}
+function toMax(){
+	let numA=inputA.value;
+	let numB=inputB.value;
+	if(isNumber(numA)&&isNumber(numB)){
+		numA=parseFloat(numA);
+	    numB=parseFloat(numB);
+	    show.innerHTML=Math.max(numA,numB);
+	}
+	else{
+		show.innerHTML="输入框的内容不是数字";
+	}
+	
+}
+function toMin(){
+	let numA=inputA.value;
+	let numB=inputB.value;
+	if(isNumber(numA)&&isNumber(numB)){
+		numA=parseFloat(numA);
+	    numB=parseFloat(numB);
+	    show.innerHTML=Math.min(numA,numB);
+	}
+	else{
+		show.innerHTML="输入框的内容不是数字";
+	}
+	
+}
+function showLength(){
+	let myRadio=document.querySelector(".string input:checked");
+	let checked=myRadio.value;
+	let myText=document.getElementById("str-"+checked).value;
+	show2.innerHTML=myText.length;
+}
+function show3rd(){
+	let myRadio=document.querySelector(".string input:checked");
+	let checked=myRadio.value;
+	let myText=document.getElementById("str-"+checked).value;
+	show2.innerHTML=myText.charAt(2);
+}
+function concatAB(){
+	let myTextA=textA.value;
+	let myTextB=textB.value;
+	show2.innerHTML=myTextA.concat(myTextB);
+}
+function searchBinA(){
+	let myTextA=textA.value;
+	let myTextB=textB.value;
+	show2.innerHTML=myTextA.indexOf(myTextB);
+}
+function searchAinB(){
+	let myTextA=textA.value;
+	let myTextB=textB.value;
+	show2.innerHTML=myTextB.lastIndexOf(myTextA);
+}
+function showAtoB(){
+	let numA=inputStrA.value;
+	let numB=inputStrB.value;
+	if(isNumber(numA)&&isNumber(numB)){
+		numA=parseInt(numA);
+		numB=parseInt(numB);
+		let myRadio=document.querySelector(".string input:checked");
+		let checked=myRadio.value;
+		let myText=document.getElementById("str-"+checked).value;
+		show2.innerHTML=myText.slice(numA,numB);
+	}
+	
+}
+//判断文本框中字符串的行数：用总字符数/文本框列数:取整
+function countRow(){
+	let myRadio=document.querySelector(".string input:checked");
+	let checked=myRadio.value;
+	let Text=document.getElementById("str-"+checked);
+	let myText=Text.value;
+	let col=Text.cols;
+	let row=(myText.length/col).toFixed(0);
+	show2.innerHTML=row;
+}
+function showChats(){
+	let numA=inputStrA.value;
+	let numB=inputStrB.value;
+	if(isNumber(numA)&&isNumber(numB)){
+		numA=parseInt(numA);
+		numB=parseInt(numB);
+		let myRadio=document.querySelector(".string input:checked");
+		let checked=myRadio.value;
+		let myText=document.getElementById("str-"+checked).value;
+		show2.innerHTML=myText.substr(numA,numB);
+	}
+}
+function toUp(){
+	let Myradio=document.querySelector(".string input:checked");
+	let checked=Myradio.value;
+	let myText=document.getElementById("str-"+checked).value;
+	show2.innerHTML=myText.toLocaleUpperCase();
+}
+function toLow(){
+	let Myradio=document.querySelector(".string input:checked");
+	let checked=Myradio.value;
+	let myText=document.getElementById("str-"+checked).value;
+	show2.innerHTML=myText.toLocaleLowerCase();
+}
+function delSpace(){
+	let Myradio=document.querySelector(".string input:checked");
+	let checked=Myradio.value;
+	let myText=document.getElementById("str-"+checked).value;
+	/*let showText="";
+	for(let i=0;i<myText.length;i++){
+		if(myText.charCodeAt(i)!=32){
+			showText+=myText.charAt(i);
+		}
+	}
+	show2.innerHTML=showText;*/
+	show2.innerHTML= myText.replace(/\u0020/g,"");
+}
+function changeA(){
+	let myRadio=document.querySelector(".string input:checked");
+    let onText,offText;
+    switch(myRadio.value){
+    	case "a":
+    	    onText=document.getElementById("str-a").value;
+    	    offText=document.getElementById("str-b").value;
+    	    break;
+    	case "b":
+    	    onText=document.getElementById("str-b").value;
+    	    offText=document.getElementById("str-a").value;
+    	    break;
+    }
+	show2.innerHTML=onText.replace(/a/g,offText);
+}
+/*
+实现一个字符串头尾去除空格的函数
+注意需要去除的空格，包括全角、半角空格
+暂时不需要学习和使用正则表达式的方式
+*/
+function diyTrim(str) {
+    let result = "";
+    /*let left=0;
+    let right=str.length-1;
+    
+    while(str.charCodeAt(left)==32||str.charCodeAt(left)==12288){
+    	left+=1;
+    }
+    while(str.charCodeAt(right)==32||str.charCodeAt(right)==12288){
+    	right-=1;
+    }
+    result=str.slice(left,right+1);*/
+    // do something
+    result = str.replace(/(^\s+|\s+$)/g,"");
+    return result;
+}
+
+// 测试用例
+console.log(diyTrim(' a f b    ')); // ->a f b
+console.log(diyTrim('    ffdaf    ')); // ->ffdaf
+console.log(diyTrim('1    ')); // ->1
+console.log(diyTrim('　　f')); // ->f
+console.log(diyTrim('  　  a f b 　　 ')); // ->a f b
+console.log(diyTrim(' ')); // ->
+console.log(diyTrim('　')); // ->
+console.log(diyTrim('')); // ->
+
+/*
+去掉字符串str中，连续重复的地方
+*/
+function removeRepetition(str) {
+    let result = "";
+    let count=0;
+    if(str==""){
+    	return "";
+    }
+    else{
+    	result+=str[count];
+	    if(str.length>1){
+	    	for(let i=1;i<str.length;i++){
+		    	if(str[i]!=result[count]){
+		    		result+=str[i];
+		    		count++;
+		    	}
+		    }
+	    }
+    }
+    
+    
+    // do something
+
+    return result;
+}
+
+// 测试用例
+console.log(removeRepetition("aaa")); // ->a
+console.log(removeRepetition("abbba")); // ->aba
+console.log(removeRepetition("aabbaabb")); // ->abab
+console.log(removeRepetition("")); // ->
+console.log(removeRepetition("abc")); // ->abc
---
+++ D22-D24/object.js
@@ -0,0 +1,367 @@
+var tree = {
+    "id": 0,
+    "name": "root",
+    "left": {
+        "id": 1,
+        "name": "Simon",
+        "left": {
+            "id": 3,
+            "name": "Carl",
+            "left": {
+                "id": 7,
+                "name": "Lee",
+                "left": {
+                    "id": 11,
+                    "name": "Fate"
+                }
+            },
+            "right": {
+                "id": 8,
+                "name": "Annie",
+                "left": {
+                    "id": 12,
+                    "name": "Saber"
+                }
+            }
+        },
+        "right": {
+            "id": 4,
+            "name": "Tony",
+            "left": {
+                "id": 9,
+                "name": "Candy"
+            }
+        }
+    },
+    "right": {
+        "id": 2,
+        "name": "right",
+        "left": {
+            "id": 5,
+            "name": "Carl",
+        },
+        "right": {
+            "id": 6,
+            "name": "Carl",
+            "right": {
+                "id": 10,
+                "name": "Kai"
+            }        
+        }
+    }
+}
+
+// 假设id和name均不会重复，根据输入name找到对应的id
+function findIdByName(myName) {
+    function find(obj){
+        if(obj.name === myName){
+            console.log(obj.id);
+            //如果在此设return返回值，那么只有在Tony的分支函数返回结束了。而其他的函数还在继续
+            //递归是多个函数的嵌套执行，一个函数的return不会让整体结束
+        }
+        else{
+        	if(obj.left){
+        		find(obj.left);
+        	}
+        	if(obj.right){
+        		find(obj.right);
+        	}
+            
+        }
+            
+        
+    }
+    find(tree);
+}
+findIdByName("Tony");
+// 假设id和name均不会重复，根据输入id找到对应的name
+function findNameById(myId) {
+    function find(obj){
+    	if(obj.id==myId){
+    		console.log(obj.id);
+    	}
+    	else{
+    		if(obj.left){
+    			find(obj.left);
+    		}
+    		if(obj.right){
+    			find(obj.right);
+    		}
+    	}
+    }
+    find(tree);
+}
+findNameById("8");
+// 把这个对象中所有的名字以“前序遍历”的方式全部输出到console中
+function getListWithDLR(obj) {
+    console.log(obj.name);
+    if(obj.left){
+    	getListWithDLR(obj.left);
+    }
+    if(obj.right){
+    	getListWithDLR(obj.right);
+    }
+}
+
+// 把这个对象中所有的名字以“中序遍历”的方式全部输出到console中
+function getListWithLDR(obj) {
+    if(obj.left){
+    	getListWithLDR(obj.left);
+    }
+    console.log(obj.name);
+    if(obj.right){
+    	getListWithLDR(obj.right);
+    }
+}
+
+// 把这个对象中所有的名字以“后序遍历”的方式全部输出到console中
+function getListWithLRD(obj) {
+    if(obj.left){
+    	getListWithLRD(obj.left);
+    }
+    if(obj.right){
+    	getListWithLRD(obj.right);
+    }
+    console.log(obj.name);
+}
+
+let rqueue = ["apple", "pear"];
+let lqueue = ["apple", "pear"];
+let quInput=document.getElementById("queue-input");
+let rShow=document.getElementById("queue-ricont");
+let lShow=document.getElementById("queue-lecont");
+function leUnShift(){
+	let myIn=quInput.value;
+	rqueue.unshift(myIn);
+	rShow.innerHTML=rqueue.join("-&gt");
+}
+function riPop(){
+	rqueue.pop();
+	rShow.innerHTML=rqueue.join("-&gt");
+}
+function printRTop(){
+	rShow.innerHTML=rqueue[rqueue.length-1];
+}
+function isrNull(){
+	if(rqueue.length!=0){
+		rShow.innerHTML="队列不为空！";
+	}
+	else{
+		rShow.innerHTML="队列为空！";
+	}
+}
+function riPush(){
+	lqueue.push(quInput.value);
+	lShow.innerHTML=lqueue.join("&lt-");
+}
+function leShift()
+{
+	lqueue.shift();
+	lShow.innerHTML=lqueue.join("&lt-");
+}
+function printLTop(){
+	lShow.innerHTML=lqueue[0];
+}
+function islNull(){
+	if(lqueue.length!=0){
+		lShow.innerHTML="队列不为空！";
+	}
+	else{
+		lShow.innerHTML="队列为空！";
+	}
+}
+let rStack = ["apple", "pear"];
+let lStack = ["apple", "pear"];
+let stInput=document.getElementById("stack-input");
+let srShow=document.getElementById("stack-rcont");
+let slShow=document.getElementById("stack-lcont");
+function pushRight(){
+	let myInput=stInput.value;
+	rStack.push(myInput);
+	srShow.innerHTML=rStack.join("-&gt");
+}
+function popRight(){
+	rStack.pop();
+	srShow.innerHTML=rStack.join("-&gt");
+}
+function printRStackTop(){
+	srShow.innerHTML=rStack[rStack.length-1];
+}
+function isRStackNull(){
+	if(rStack.length!=0){
+		srShow.innerHTML="队列不为空！";
+	}
+	else{
+		srShow.innerHTML="队列为空！";
+	}
+}
+function pushLeft(){
+	let myInput=stInput.value;
+	lStack.unshift(myInput);
+	slShow.innerHTML=lStack.join("&lt-");
+}
+function popLeft(){
+	lStack.shift();
+	slShow.innerHTML=lStack.join("&lt-");
+}
+function printLStackTop(){
+	slShow.innerHTML=lStack[0];
+}
+function isLStackNull(){
+	if(lStack.length!=0){
+		slShow.innerHTML="队列不为空！";
+	}
+	else{
+		slShow.innerHTML="队列为空！";
+	}
+}
+let arr1 = [43, 54, 4, -4, 84, 100, 58, 27, 140];
+let result1=document.getElementById("rank-1-result");
+function sortStoB(){
+	result1.innerHTML= arr1.sort(function(a,b){
+		return a-b;
+	})
+	
+}
+function sortBtoS(){
+	result1.innerHTML= arr1.sort(function(a,b){
+		return b-a;
+	})
+}
+let arr2 = ['apple', 'dog', 'cat', 'car', 'zoo', 'orange', 'airplane'];
+let result2=document.getElementById("rank-2-result");
+function sortatoZ(arr){
+	result2.innerHTML=arr.sort();
+}
+function sortZtoa(arr){
+	result2.innerHTML=arr.sort(function(a,b){
+		return b.localeCompare(a);
+	})
+}
+let arr3 = [[10, 14], [16, 60], [7, 44], [26, 35], [22, 63]];
+let result3=document.getElementById("rank-3-result");
+function sortBy2nd(){
+	arr3=arr3.sort(function(a,b){
+		return b[1]-a[1];
+	})
+	result3.innerHTML="[["+arr3.join("],[")+"]]";
+}
+let arr4 = [
+    {
+        id: 1,
+        name: 'candy',
+        value: 40
+    }, {
+        id: 2,
+        name: 'Simon',
+        value: 50
+    }, {
+        id: 3,
+        name: 'Tony',
+        value: 45
+    }, {
+        id: 4,
+        name: 'Annie',
+        value: 60
+    }
+];
+let result4=document.getElementById("rank-4-result");
+function sortByValue(){
+	arr4=arr4.sort(function(a,b){
+		
+		if (!isNaN(a.value)&&(!isNaN(b.value))){
+			let numA=parseInt(a.value);
+			let numB=parseInt(b.value);
+			return numA-numB;
+		}
+	})
+	//console.log(arr4);控制台可以输出对象，但无法直接显示在innerHTML中
+	let strArr4=arr4.map(function (item,index,arr){
+		return JSON.stringify(item);
+	})
+	result4.innerHTML=strArr4;
+}
+let scoreObject = {
+    "Tony": {
+        "Math": 95,
+        "English": 79,
+        "Music": 68
+    }, 
+    "Simon": {
+        "Math": 100,
+        "English": 95,
+        "Music": 98
+    }, 
+    "Annie": {
+        "Math": 54,
+        "English": 65,
+        "Music": 88
+    }
+}
+let scoreArray=[];
+for (let propName in scoreObject){
+	let temp=[];
+	let tempObj=scoreObject[propName];
+	temp.push(propName);
+	for(let pro in tempObj){
+		temp.push(tempObj[pro]);
+	}
+	scoreArray.push(temp);
+}
+console.log(scoreArray);
+let menuArr = [
+    [1, "Area1", -1],
+    [2, "Area2", -1],
+    [3, "Area1-1", 1],
+    [4, "Area1-2", 1],
+    [5, "Area2-1", 2],
+    [6, "Area2-2", 2],
+    [7, "Area1-2-3", 4],
+    [8, "Area2-2-1", 6],
+];
+let menuObject=new Object();
+let root={};
+//定义一个对象，用来表示节点之间的关系
+menuArr.forEach(function(arr){
+	//先找第一层，arr[2]=-1，
+	if(arr[2]==-1){
+		let myKey=arr[0];
+		menuObject[myKey]={};
+		menuObject[myKey].name=arr[1];
+		menuObject[myKey].subMenu={};
+		root[arr[0]]={};
+	}
+	//如果不在第一层，看一下是否在第二层，不过前提是第一层的节点已经找到了，所以这个算法对数组顺序有要求
+	//给定数组基本是按照层数来的，所以该算法不会出问题，但如果打乱顺序，会出现前面节点没找到后面节点都无法找到位置的情况
+	if(root!=null){
+		for(var prop in root){
+			if(arr[2]==prop){
+				let tempObj=menuObject[prop].subMenu;
+				let tempName=arr[0];
+				tempObj[tempName]={};
+				tempObj[tempName].name=arr[1];
+				tempObj[tempName].subMenu={};
+				root[prop][arr[0]]={};
+			}
+			//如果也不在第二层，就找第三层
+			if(root[prop]!=null){
+				for(var subPro in root[prop]){
+					if(arr[2]==subPro){
+						let tempObj=menuObject[prop].subMenu;
+						tempObj=tempObj[subPro].subMenu;
+						tempName=arr[0];
+						tempObj[tempName]={};
+						tempObj[tempName].name=arr[1];
+					}
+				}
+			}
+		}
+		
+		
+	}
+	
+	
+})
+console.log(menuObject);
+//for ...in
+//arr.forEach()
