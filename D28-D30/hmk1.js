47c6ce88207e105abb0bd9ce634a118e888cba38
holichang <1185381333@qq.com>
2019-09-19 10:49:40 +0800

    update D28-D30

--- D28-D30/hmk1.js
+++ D28-D30/hmk1.js
@@ -4,14 +4,30 @@ var postfixList = ['163.com', 'gmail.com', '126.com', 'qq.com', '263.net'];
 let inputDom=document.getElementById("email-input");
 let emailWrapper=document.getElementById("email-sug-wrapper");
 inputDom.addEventListener("input",addHint);
+let liList=document.querySelectorAll("#email-sug-wrapper li");
+liClick();
 //inputDom.addEventListener("keydown",addHint);
 //inputDom.addEventListener("keyup",addHint);
 //inputDom.addEventListener("keypress",addHint);
+
 function addHint(){
 	clearUl();
 	setUl();
 	console.log("event handle!");
 }
+//定义li的点击事件，使用事件代理，减少与dom的操作
+function liClick(){
+	emailWrapper.addEventListener("click",function(ev){
+		ev=event||window.event;
+		myTarget=ev.target||ev.srcElement;
+		if(myTarget.nodeName.toLowerCase()=="li"){
+			//inputDom.value=myTarget.innerText;
+			inputDom.value=encoding(myTarget.innerHTML);
+			emailWrapper.style.display="none";
+		}
+	});
+}
+//该函数用于每次事件中先清除ul中的内容
 function clearUl(){
 	let myLen=emailWrapper.childNodes.length;
 	for(let i=myLen-1;i>=0;i--){
@@ -21,6 +37,8 @@ function clearUl(){
 //设置ul下的值
 function setUl(){
 	let myInput=inputDom.value.trim();
+	myInput=coding(myInput);
+	console.log(myInput);
 	let emailList;
 	isVisiblity(myInput);
 	if(myInput.indexOf("@")==-1){
@@ -35,6 +53,7 @@ function setUl(){
 		emLi.innerHTML=myInput+"@"+emailList[i];
 		emailWrapper.appendChild(emLi);
 	}
+	emailWrapper.childNodes[0].className="active";
 }
 //根据输入框中的值trim后是否是空字符串控制ul是否可见
 function isVisiblity(input){
@@ -46,7 +65,7 @@ function isVisiblity(input){
 	}
 }
 //对于不合法的字符串，截取@前面的值
-// 根据
+// 根据输入的值判断提示邮箱的格式
 function inter(str){
 	let arr=[];
 	bef=str.split("@");
@@ -70,5 +89,17 @@ function inter(str){
 	{
 		arr.push('163.com', 'gmail.com', '126.com', 'qq.com', '263.net');
 	}
+	return arr;
+}
+function coding(arr){
+	arr=arr.replace("<","&lt;");
+	arr=arr.replace(">","&gt;");
+	//arr=arr.replace("&","&amp;");
+	return arr;
+}
+function encoding(arr){
+	arr=arr.replace("&lt;","<");
+	arr=arr.replace("&gt;",">");
+	//arr=arr.replace("&amp;","&");
 	return arr;
 }
